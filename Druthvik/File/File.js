dbab20d6ef4f7efb30e1f520ffec8a041ecd3131
druthvik@electems.com <druthvik@electems.com>
2022-06-29 11:18:49 +0530

    append commit

--- Druthvik/File/File.js
+++ Druthvik/File/File.js
@@ -1,14 +1,14 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-var data = fs.readFileSync('largefile.txt').toString('utf-8');
+var data = fs.readFileSync('datefile.txt').toString('utf-8');
 {
     var textByLine = data.split('\n');
     console.log(textByLine);
 }
 var loop = textByLine.forEach(function (data) {
-    var firstRow = data.slice(0, data.indexOf('\r\n\r\n')); // get "Name: XX"
-    var name = firstRow.split(': ')[1]; // get "XX
+    var firstRow = data.slice(0, data.indexOf(/d{2}(D)d{2}\1d{4}/g)); // get "Name: XX"
+    var name = firstRow.split(': ')[0]; // get "XX
     var file = 'write and read';
     fs.exists("../File/".concat(name), function (exist) {
         if (exist) {
--- Druthvik/File/File.ts
+++ Druthvik/File/File.ts
@@ -1,14 +1,14 @@
 import * as fs from 'fs'
 
-let data = fs.readFileSync('largefile.txt').toString('utf-8')
+let data = fs.readFileSync('datefile.txt').toString('utf-8')
 {
   var textByLine = data.split('\n')
   console.log(textByLine)
 }
 
 var loop = textByLine.forEach((data: any) => {
-  var firstRow = data.slice(0, data.indexOf('\r\n\r\n')) // get "Name: XX"
-  var name = firstRow.split(': ')[1] // get "XX
+  var firstRow = data.slice(0, data.indexOf(/d{2}(D)d{2}\1d{4}/g)) // get "Name: XX"
+  var name = firstRow.split(': ')[0] // get "XX
 
   const file = 'write and read'
   fs.exists(`../File/${name}`, (exist: any) => {
