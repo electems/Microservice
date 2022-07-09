d294b840c9102b2b5ebb2429e884dd6f686acbaa
druthvik@electems.com <druthvik@electems.com>
2022-07-09 11:26:30 +0530

    ff

---
+++ Druthvik/studentmanagement/studentsranking/rank.allotment.controller.ts
@@ -0,0 +1,93 @@
+import { Body, Get, Post, Route, Tags } from 'tsoa';
+import { getRepository } from 'typeorm';
+import { Student } from './model/student';
+import { RankAllotment } from './rank_allotment';
+
+const student: Student[] = [
+  {
+    name: 'John',
+    subjects: [
+      {
+        name: 'Maths',
+        semester: '2',
+        mark: {
+          scoredmarks: 67,
+          maxmarks: 125,
+        },
+      },
+      {
+        name: 'science',
+        semester: '2',
+        mark: {
+          scoredmarks: 68,
+          maxmarks: 127,
+        },
+      },
+    ],
+  },
+  {
+    name: 'kennedy', //loaded student array
+    subjects: [
+      {
+        name: 'social',
+        semester: '2',
+        mark: {
+          scoredmarks: 90,
+          maxmarks: 125,
+        },
+      },
+      {
+        name: 'english',
+        semester: '2',
+        mark: {
+          scoredmarks: 80,
+          maxmarks: 127,
+        },
+      },
+    ],
+  },
+];
+@Route('students')
+@Tags('Student')
+export default class RankAllotmentController {
+  // @Get('/')
+  // public async getStudents(): Promise<Array<Student>> {
+  //   return student
+  // }
+
+  @Post('/')
+  public async createStudent(@Body() body: Student): Promise<Student> {
+   return (body)
+  }
+
+  @Get('/')
+  public async getStudentAverage():  Promise<Array<Student>> {
+    const getStudent = new RankAllotment(student)
+    getStudent.calculateAvgOfAllTheStudents()
+    const data = student
+    data[0].avgMarks 
+    data[1].avgMarks
+    return data
+  }
+
+  
+
+  // @Post('/')
+  // public async createStudentWithAverage(@Body() body: Student): Promise<Student> {
+  //   const getStudent = new RankAllotment(student)
+  //   const avg = getStudent.calculateAvgOfAllTheStudents
+  // }
+
+  
+  // @Get('/')
+  // public async getStudentsAverage(): Promise<Array<Student>> {
+  //   const getStudent = new RankAllotment(student)
+  //   const avg = getStudent.sortByAverage()
+  //   return avg
+  // }
+
+}
+
+
+
+
---
+++ Druthvik/studentmanagement/studentsranking/rankallotment.router.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import RankAllotmentController from '../studentsranking/rank.allotment.controller'
+const router = express.Router();
+
+// router.get('/', async (_req, res) => {
+//     const controller = new RankAllotmentController();
+//     const response = await controller.getStudents();
+//     return res.send(response);
+//   });
+
+router.get('/', async (_req, res) => {
+  const controller = new RankAllotmentController();
+  const response = await controller.getStudentAverage();
+  return res.send(response);
+});
+
+  router.post('/', async (req, res) => {
+    const controller = new RankAllotmentController();
+    const response = await controller.createStudent(req.body);
+    return res.send(response);
+});
+
+
+  
+
+// router.get('/', async (_req, res) => {
+//     const controller = new RankAllotmentController();
+//     const response = await controller.getStudentsAverage();
+//     return res.send(response);
+//   });
+
+  export default router;
