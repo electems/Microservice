499505042a592d4258a4a2137deb934ec9b02a70
vasavielectem <vasavi@electems.com>
2022-07-15 18:11:00 +0530

    todays work

---
+++ shashi/students/studentRanking/ranking.test.ts
@@ -0,0 +1,66 @@
+import { students } from "../models/student";
+import{StudentRank} from"../studentRanking/ranking";
+const student: students[] = [
+  {
+    "name": "John",
+    "subject": [
+      {
+        "name": "Maths",
+
+        "semester":"2",
+
+        "marks": {
+          "scoredMarks": 50,
+
+          "maxMarks": 125,
+        },
+      },
+
+      {
+        "name": "science",
+
+        "semester": "2",
+
+        "marks": {
+            "scoredMarks": 50,
+
+            "maxMarks": 127,
+        },
+      },
+    ],
+  },
+
+  {
+    "name": "kennedy", //loaded student array
+
+    "subject": [
+      {
+        "name": "social",
+
+        "semester":"2",
+
+        "marks": {
+            "scoredMarks": 68,
+
+            "maxMarks": 127,
+        },
+      },
+
+      {
+        "name": "english",
+
+        "semester":"2",
+
+        "marks": {
+        "scoredMarks": 80,
+        "maxMarks": 100
+        },
+      },
+    ],
+  },
+];
+
+const ranking = new StudentRank(student);
+ranking.AvgMarksCalculation();
+expect(student[0].Avg_marks).toBe(50);
+expect(student[1].Avg_marks).toBe(74);
---
+++ shashi/students/studentRanking/ranking.ts
@@ -0,0 +1,21 @@
+import { students } from "../models/student";
+import { Mark } from "../models/mark";
+
+export class StudentRank {
+  studentlist: students[] = [];
+
+  constructor(studentlist: students[]) {
+    this.studentlist = studentlist;
+  }
+
+  AvgMarksCalculation(): void {
+    for (const student of this.studentlist) {
+      const subject = student.subject;
+      let TotalMark = 0;
+      for (const subjects of subject) {
+        TotalMark += subjects.marks.scoredMarks;
+      }
+      student.Avg_marks = TotalMark / student.subject.length;
+    }
+  }
+}
