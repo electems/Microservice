903d3562f6d53c90fc6802673c21841a9bc46e22
shivangivy <shivangi@electems.com>
2022-07-19 18:22:47 +0530

    code commit for Stringutil execis

---
+++ adarsh/StringTest.java
@@ -0,0 +1,13 @@
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		StringConcatination1 sc = new StringConcatination1();
+		System.out.println(sc.compareTwoStrings("angadi","adasrsh"));
+		System.out.println(sc.occurrenceOfCharacter("Adarsh", 'd'));
+		System.out.println(sc.removeWhiteSpace("A d a r s h"));
+		System.out.println(sc.findGmailString("null,a@gmail.com,null,b@redbus.com,c@gmail.com,d@gmail.com"));
+		 
+	}
+
+}
