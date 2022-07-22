9512365369050d753343e46ca4f9a1d885a9e324
HarshaMC11 <harsha@electems.com>
2022-07-22 18:25:24 +0530

    Code on Find method

---
+++ Harsha/MapFindFilterReduce/costumerData.java
@@ -0,0 +1,22 @@
+package com.collection;
+
+public class costumerData {
+	    int bill;
+	    String name;
+	    String item;
+	    public costumerData(String name, int bill, String item) {
+	        this.name = name;
+	        this.bill = bill;
+	    }
+		public String getItem() {
+			return item;
+		}
+		public int getbill() {
+			return bill;
+		}	
+		public String getName() {
+			return name;
+		}
+
+		}
+
