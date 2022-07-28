c130983d6f23d8f66152f786a5847f799ea53671
shubhankar54 <shubhankar@electems.com>
2022-07-28 15:54:44 +0530

    v

--- shubhankar/charAt.ts
+++ shubhankar/charAt.ts
@@ -1,3 +1,18 @@
-var n = new String ('this is the new string')
+// // var n = new String ('this is the new string')
 
-console.log(n.charAt(3))
+// // console.log(n.charAt(3))
+
+// const fs = require('fs');
+
+// fs.appendFile('message.txt', 'data to append', function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+const fs = require('fs');
+  
+// Calling the readFileSync() method
+// to read 'input.txt' file
+const data = fs.readFileSync('./run.txt', {encoding:'utf8'});
+ 
+// Display the file data
+console.log(data);
