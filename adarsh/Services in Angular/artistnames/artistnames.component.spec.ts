07eb2c8e56f1b4371d84e6aadc3b4ae5797a88e1
adarshangadi64 <adarsh@electems.com>
2022-08-02 17:54:14 +0530

    commit on Use Services in Angular

---
+++ adarsh/Services in Angular/artistnames/artistnames.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArtistnamesComponent } from './artistnames.component';
+
+describe('ArtistnamesComponent', () => {
+  let component: ArtistnamesComponent;
+  let fixture: ComponentFixture<ArtistnamesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArtistnamesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArtistnamesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
