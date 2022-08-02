451bf81aeec590a4859e455b8cfc92895fdbdc18
HarshaMC11 <harsha@electems.com>
2022-08-02 18:04:33 +0530

    commit on angular service class

--- Harsha/Angular/app/app.module.ts
+++ Harsha/Angular/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';    
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { ArtistnamesComponent } from './artistnames/artistnames.component';
+import { DataService } from './data.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistsComponent,
+    ArtistnamesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Harsha/Angular/app/artists/artists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+@Component({
+  selector: 'app-artists',
+  templateUrl: './artists.component.html',
+  styleUrls: ['./artists.component.css']
+})
+export class ArtistsComponent implements OnInit {
+public artists:any = []
+constructor(private list: DataService) { }
+ngOnInit(): void {
+    this.artists = this.list.getList();
+    console.log("Working fine");
+  }
+}
