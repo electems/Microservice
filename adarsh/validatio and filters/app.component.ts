be8f5345b8667757d05a7730d9d695e87c5b4c11
adarshangadi64 <adarsh@electems.com>
2022-08-05 18:14:56 +0530

    commit on filters

---
+++ adarsh/validatio and filters/app.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from "@angular/core";
+import { User } from "./models/user";
+import { UserService } from "./services/user.service";
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  FormArray,
+} from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent implements OnInit {
+  title: "title";
+  users: User[];
+  states: any;
+  userForm: boolean;
+  isNewUser: boolean;
+  newUser: any = {};
+  editUserForm: boolean;
+  editedUser: any = {};
+  registerForm: FormGroup;
+  filterUser: User[];
+  private searchText: string;
+
+  set searchTerm(value: string) {
+    this.searchText = value;
+    if (this.searchText == "") {
+      this.filterUser = this.users;
+    } else {
+      this.filterUser = this.getFilteredList(value);
+    }
+  }
+
+  getFilteredList(searchText: string) {
+    return this.filterUser.filter((user) => {
+      return user.firstName.toLowerCase().includes(searchText);
+    });
+  }
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.users = this.getUsers();
+    this.states = this.getStates();
+    this.filterUser = this.users;
+  }
+
+  getStates() {
+    return this.userService.getUsersStates();
+  }
+  getUsers(): User[] {
+    return this.userService.getUsersFromData();
+  }
+
+  showEditUserForm(user: User) {
+    this.userForm = false;
+    if (!user) {
+      this.userForm = false;
+      return;
+    }
+    this.editUserForm = true;
+    this.editedUser = user;
+  }
+
+  showAddUserForm() {
+    // resets form if edited user
+    this.editUserForm = false;
+    if (this.users.length) {
+      this.newUser = {};
+    }
+    this.userForm = true;
+    this.isNewUser = true;
+  }
+
+  saveUser(user: User) {
+    if (this.isNewUser) {
+      // add a new user
+      this.userService.addUser(user);
+    }
+    this.userForm = false;
+  }
+
+  updateUser() {
+    this.userService.updateUser(this.editedUser);
+    this.editUserForm = false;
+    this.editedUser = {};
+  }
+
+  removeUser(user: User) {
+    this.userService.deleteUser(user);
+  }
+
+  cancelEdits() {
+    this.editedUser = {};
+    this.editUserForm = false;
+  }
+
+  cancelNewUser() {
+    this.newUser = {};
+    this.userForm = false;
+  }
+}
