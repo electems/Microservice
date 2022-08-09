5042f4281285681daa29f079c5a0d23776a25b35
shubhankar54 <shubhankar@electems.com>
2022-08-09 18:09:16 +0530

    Student form new

---
+++ shubhankar/StudentFormnew/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component'
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component'
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component'
+import { StudentComponent } from './components/student/student.component'
+import { CourceComponent } from './components/cource/cource.component'
+import { StudentListComponent } from './components/student-list/student-list.component'
+import { CourceListComponent } from './components/cource-list/cource-list.component'
+import { DemoListComponent } from './components/demo-list/demo-list.component'
+import { StudentDetailsComponent } from './student-details/student-details.component'
+import { CourceDetailsComponent } from './components/cource-details/cource-details.component'
+import { DemoDetailsComponent } from './components/demo-details/demo-details.component'
+import { TestComponent } from './components/test/test.component'
+import { NgSelectModule } from '@ng-select/ng-select'
+import { ProductComponent } from './components/product/product.component'
+import { UserComponent } from './components/user/user.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { StudentformComponent } from './studentform/studentform.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent,
+    StudentComponent,
+    CourceComponent,
+    StudentListComponent,
+    CourceListComponent,
+    DemoListComponent,
+    StudentDetailsComponent,
+    CourceDetailsComponent,
+    DemoDetailsComponent,
+    TestComponent,
+    ProductComponent,
+   UserComponent,
+    UserListComponent,
+    StudentformComponent,
+    EditStudentComponent,
+    
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgSelectModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
