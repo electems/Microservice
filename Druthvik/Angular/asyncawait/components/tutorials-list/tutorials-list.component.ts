76abae81a7a0adf79628ecd1446db170439b5675
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-08-10 12:10:34 +0530

    async and await

---
+++ Druthvik/Angular/asyncawait/components/tutorials-list/tutorials-list.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from 'src/app/models/tutorial.model';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+@Component({
+  selector: 'app-tutorials-list',
+  templateUrl: './tutorials-list.component.html',
+  styleUrls: ['./tutorials-list.component.css'],
+})
+export class TutorialsListComponent implements OnInit {
+  tutorials?: any;
+  currentTutorial?: Tutorial;
+  currentIndex = -1;
+  title = '';
+  page = 1;
+  count = 0;
+  pageSize = 3;
+
+  constructor(private tutorialService: TutorialService) {}
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+    this.title = 'helllo';
+    // this.tutorials = [
+    //   { title: 'some1', description: 'dd' },
+    //   { title: 'some2', description: 'dd' },
+    //   { title: 'some1', description: 'dd' },
+    //   { title: 'some2', description: 'dd' },
+    //   { title: 'some1', description: 'dd' },
+    //   { title: 'some2', description: 'dd' },
+    // ];
+  }
+
+  handlePageChange(event: any): void {
+    this.page = event;
+    this.retrieveTutorials();
+  }
+
+  handlePageSizeChange(event: { target: { value: number } }): void {
+    this.pageSize = event.target.value;
+    this.page = 1;
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll().subscribe(
+      (data) => {
+        this.tutorials = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  refreshList(): void {
+    this.retrieveTutorials();
+    this.currentTutorial = undefined;
+    this.currentIndex = -1;
+  }
+
+  setActiveTutorial(tutorial: Tutorial, index: number): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll().subscribe(
+      (response) => {
+        console.log(response);
+        this.refreshList();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  searchTitle(): void {
+    this.currentTutorial = undefined;
+    this.currentIndex = -1;
+
+    this.tutorialService.findByTitle(this.title).subscribe(
+      (data) => {
+        this.tutorials = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
