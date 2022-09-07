c2541c787fd4679f848129271de4fbb735ef1a18
shubhankar54 <shubhankar@electems.com>
2022-09-07 15:29:26 +0530

    bootstrap

---
+++ shubhankar/bootstrap/tutorialtable/tutorialtable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorialtable',
+  templateUrl: './tutorialtable.component.html',
+  styleUrls: ['./tutorialtable.component.css']
+})
+export class TutorialtableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
